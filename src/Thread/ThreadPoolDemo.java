6a25719f3cf083ee829c2555b75d300f01cfd043
liwei <401613869@qq.com>
2022-04-20 20:27:01 +0800

    volg5

--- src/Thread/ThreadPoolDemo.java
+++ src/Thread/ThreadPoolDemo.java
@@ -1,5 +1,9 @@
 package Thread;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * @author liwei
  * @create 2022-04-20-16:07
@@ -9,4 +13,26 @@ package Thread;
  * 2:控制线程数量
  **/
 public class ThreadPoolDemo {
+    public static void main(String[] args) {
+        /*
+        JUC java.util.concurrent包
+        concurrent 并发
+        java的并发包，里面都是关于线程的api,线程池就在这个包里
+         */
+        ExecutorService threadPool = Executors.newFixedThreadPool(2);
+        for (int i = 0; i < 5; i++) {
+            threadPool.execute(new Runnable() {
+                @Override
+                public void run() {
+                    System.out.println(Thread.currentThread().getName()+"正在执行任务...");
+                    try {
+                        Thread.sleep(5000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    System.out.println(Thread.currentThread().getName()+"执行完毕");
+                }
+            });
+        }
+    }
 }
