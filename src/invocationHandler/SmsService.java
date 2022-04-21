4441f268b987daf63fe77f7969d693981ab9b244
liwei <401613869@qq.com>
2022-04-21 09:54:40 +0800

    volg5

---
+++ src/invocationHandler/SmsService.java
@@ -0,0 +1,13 @@
+package invocationHandler;
+
+/**
+ * @author liwei
+ * @create 2022-04-21-9:54
+ *  JDK 动态代理类使用步骤
+ *
+ *     定义一个接口及其实现类；
+ *     自定义 InvocationHandler 并重写invoke方法，在 invoke 方法中我们会调用原生方法（被代理类的方法）并自定义一些处理逻辑；
+ *     通过 Proxy.newProxyInstance(ClassLoader loader,Class<?>[] interfaces,InvocationHandler h) 方法创建代理对象；
+ **/
+public class SmsService {
+}
